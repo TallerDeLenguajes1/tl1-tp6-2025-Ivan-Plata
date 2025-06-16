5d02e2306dbeb511ab4ba3526ceb89a711c80403
Ivan-Plata <ivanplataunt@gmail.com>
2025-06-16 19:05:28 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+string numero = "1234506";
+string invertido = "";
+
+int i = 0;
+
+bool bandera = int.TryParse(numero, out i);
+
+if (bandera && i > 0)
+{
+    foreach (char item in numero)
+    {
+        invertido = item + invertido;
+    }
+    Console.WriteLine("El numero invertido es: " + invertido);
+}
+else
+{
+    Console.WriteLine("\nLo ingresado no es válido");
+}
+
+
