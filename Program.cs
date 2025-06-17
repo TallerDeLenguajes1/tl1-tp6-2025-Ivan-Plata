f6c2004e17407b7a76a420eb73b3e01e4596b8f2
Ivan-Plata <ivanplataunt@gmail.com>
2025-06-16 22:43:18 -0300

    Ejercicio 4

--- Program.cs
+++ Program.cs
@@ -1,22 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-string numero = "1234506";
-string invertido = "";
+﻿using System.Diagnostics;
 
-int i = 0;
+Console.WriteLine("Ingrese una cadena de caracteres: ");
+string cadena = Console.ReadLine();
 
-bool bandera = int.TryParse(numero, out i);
+int longitud = cadena.Length;
 
-if (bandera && i > 0)
+Console.WriteLine("Longitud: " + longitud);
+
+Console.WriteLine("Ingrese otra cadena de caracteres: ");
+string cadena2 = Console.ReadLine();
+
+string concatenado = cadena + " " + cadena2;
+
+string subcadena = cadena2.Substring(0, cadena2.Length / 2);
+
+Console.WriteLine("----------CALCULADORA--------");
+Console.WriteLine("1. Suma");
+Console.WriteLine("2. Resta");
+Console.WriteLine("3. Multiplicacion");
+Console.WriteLine("4. Division");
+
+string operacion;
+bool bandera;
+int op;
+
+do
 {
-    foreach (char item in numero)
-    {
-        invertido = item + invertido;
-    }
-    Console.WriteLine("El numero invertido es: " + invertido);
+    Console.Write("Seleccione la operacion a realizar: ");
+    operacion = Console.ReadLine();
+    bandera = int.TryParse(operacion, out op);
+} while (op > 4 || op < 1 || !bandera);
+
+Console.Write("Ingrese el primer numero: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el segundo numero: ");
+int n2 = int.Parse(Console.ReadLine());
+int resultado;
+
+switch (operacion)
+{
+    case "1":
+        resultado = n1 + n2;
+        Console.WriteLine($"La suma de {n1} y de {n2} es {resultado}");
+        break;
+    case "2":
+        resultado = n1 - n2;
+        Console.WriteLine($"La resta de {n1} y de {n2} es {resultado}");
+
+        break;
+    case "3":
+        resultado = n1 * n2;
+        Console.WriteLine($"La multiplicacion de {n1} y de {n2} es {resultado}");
+
+        break;
+    case "4":
+        resultado = n1 / n2;
+        Console.WriteLine($"La division de {n1} y de {n2} es {resultado}");
+
+        break;
+}
+
+foreach (char letra in cadena)
+{
+    Console.WriteLine(letra);
+}
+
+Console.Write("Ingrese la palabra que desea buscar: ");
+string palabra = Console.ReadLine();
+
+if (cadena.Contains(palabra))
+{
+    Console.WriteLine("Si contiene.");
 }
 else
 {
-    Console.WriteLine("\nLo ingresado no es válido");
+    Console.WriteLine("No contiene.");
 }
+
+cadena = cadena.ToUpper();
+cadena = cadena.ToLower();
+
+
+string cadenaSeparar = "ivan,matias,plata";
+string[] cadenaSeparada = cadenaSeparar.Split(',');
+
+Console.Write(cadenaSeparada);
 
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/24pP-Pw_)
+
+Ejercicio 4
+
+String es un tipo por referencia
+
+\n	Nueva línea
+\r	Retorno de carro
+\t	Tabulación
+\\	Barra invertida 
+\"	Comillas dobles 
+\'	Comillas simples 
+\0	Null character
+\a	Beep 
+\b	Retroceso
+\f	Avance de página
+\v	Tabulación vertical
+
+@ ignora las secuencias de escape
+$ permite insertar variables dentro de una cadena de caracteres
