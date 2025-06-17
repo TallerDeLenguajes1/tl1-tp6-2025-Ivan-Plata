9552ddc142939d601ac33a8e69a3fc1e999ac254
Ivan-Plata <ivanplataunt@gmail.com>
2025-06-16 21:39:13 -0300

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-string numero = "1234506";
-string invertido = "";
+﻿Console.WriteLine("----------CALCULADORA--------");
+Console.WriteLine("1. Suma");
+Console.WriteLine("2. Resta");
+Console.WriteLine("3. Multiplicacion");
+Console.WriteLine("4. Division");
 
-int i = 0;
+string operacion;
+bool bandera;
+int op;
 
-bool bandera = int.TryParse(numero, out i);
-
-if (bandera && i > 0)
+do
 {
-    foreach (char item in numero)
-    {
-        invertido = item + invertido;
-    }
-    Console.WriteLine("El numero invertido es: " + invertido);
-}
-else
+    Console.Write("Seleccione la operacion a realizar: ");
+    operacion = Console.ReadLine();
+    bandera = int.TryParse(operacion, out op);
+} while (op > 4 || op < 1 || !bandera);
+
+Console.Write("Ingrese el primer numero: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el segundo numero: ");
+int n2 = int.Parse(Console.ReadLine());
+int resultado;
+
+switch (operacion)
 {
-    Console.WriteLine("\nLo ingresado no es válido");
+    case "1":
+        resultado = n1 + n2;
+        Console.WriteLine("Resultado: " + resultado);
+        break;
+    case "2":
+        resultado = n1 - n2;
+        Console.WriteLine("Resultado: " + resultado);
+        break;
+    case "3":
+        resultado = n1 * n2;
+        Console.WriteLine("Resultado: " + resultado);
+        break;
+    case "4":
+        resultado = n1 / n2;
+        Console.WriteLine("Resultado: " + resultado);
+        break;
 }
-
-
