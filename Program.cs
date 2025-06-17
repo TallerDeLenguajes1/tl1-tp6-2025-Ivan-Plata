692654fae764d7c9e753c2c18ac32ce70968226c
Ivan-Plata <ivanplataunt@gmail.com>
2025-06-16 22:03:46 -0300

    Ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-string numero = "1234506";
-string invertido = "";
-
-int i = 0;
-
-bool bandera = int.TryParse(numero, out i);
-
-if (bandera && i > 0)
-{
-    foreach (char item in numero)
-    {
-        invertido = item + invertido;
-    }
-    Console.WriteLine("El numero invertido es: " + invertido);
-}
-else
-{
-    Console.WriteLine("\nLo ingresado no es válido");
-}
+﻿
+Console.Write("Ingrese un numero: ");
+float numero = float.Parse(Console.ReadLine());
 
 
+Console.WriteLine(Math.Abs(numero));
+Console.WriteLine(numero * numero);
+Console.WriteLine(Math.Sqrt(numero));
+Console.WriteLine(Math.Sin(numero));
+Console.WriteLine(Math.Cos(numero));
+Console.WriteLine((int)numero);
+
+Console.Write("Ingrese el primer numero: ");
+float n1 = float.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo numero: ");
+float n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("El maximo es: " + Math.Max(n1, n2));
+Console.WriteLine("El minimo es: " + Math.Min(n1, n2));
